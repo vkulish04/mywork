ab92aa54701fb1f3e9500341a54c962aaaa62d99
Vlad Kulish <vkulish04@gmail.com>
2020-10-16 16:29:39 +0300

    Us

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -7,11 +7,11 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1></h1>
+        <h1>Congratulations!</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>git 
     </div>
 
     <div class="body-content">
